121067702263232bd75be1aeba4770f720110f70
firdaus <mail4frenz@gmail.com>
2020-11-14 13:58:38 +0800

    add audit interceptor

---
+++ src/Infra/Data/AuditInterceptor.cs
@@ -0,0 +1,34 @@
+﻿using AppCore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Data
+{
+    public class AuditInterceptor : SaveChangesInterceptor
+    {
+        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+        {
+            var auditableEntities =  eventData.Context.ChangeTracker.Entries<AuditableEntity>();
+
+            foreach (var entry in auditableEntities)
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.Created = DateTime.Now;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModified = DateTime.Now;
+                        break;
+                }
+            }
+
+            return base.SavingChanges(eventData, result);
+        }
+    }
+}
